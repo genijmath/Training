28710eaf440ffd6dcf69653ebd3411d0f7119761
Yevgen Yampolskiy <yevgen@chrubuntu.(none)>
2014-03-23 12:55:29 -0400

    convex hull added

---
+++ src/main/java/algo/Geometry.java
@@ -0,0 +1,11 @@
+package algo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yevgen
+ * Date: 3/23/14
+ * Time: 12:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Geometry {
+}
---
+++ src/test/java/algo/GeometryTest.java
@@ -0,0 +1,17 @@
+package algo;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yevgen
+ * Date: 3/23/14
+ * Time: 12:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GeometryTest {
+    @Test
+    public void testConvexHull() throws Exception {
+
+    }
+}
