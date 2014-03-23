b8c058a42b1de8b1c360a31c48c38b0ab8dbbe2f
Yevgen Yampolskiy <yevgen@chrubuntu.(none)>
2014-03-23 13:54:50 -0400

    simple math added

---
+++ src/test/java/algo/SimpleMathTest.java
@@ -0,0 +1,37 @@
+package algo;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yevgen
+ * Date: 3/23/14
+ * Time: 1:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleMathTest {
+    @Test
+    public void testBinomialsMatrix() throws Exception {
+
+    }
+
+    @Test
+    public void testBinomialRow() throws Exception {
+
+    }
+
+    @Test
+    public void testPrimes() throws Exception {
+
+    }
+
+    @Test
+    public void testGcd() throws Exception {
+
+    }
+
+    @Test
+    public void testLcm() throws Exception {
+
+    }
+}
